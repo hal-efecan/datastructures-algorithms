6c1b0dac2ea89d7aec09372f886120daa3520cc8
hal-efecan <hefecan@gmail.com>
2020-03-17 22:30:50 +0000

    % Completed Updated

--- datastructures-alogrithms/percCompleted.js
+++ datastructures-alogrithms/percCompleted.js
@@ -5,11 +5,11 @@
 */
 
 completion = [
-    {name: 'Basic JS',          a: 109, q: 110},
-    {name: 'ES6',               a: 17,  q: 31},
-    {name: 'REGEX',             a: 0,   q: 33},
-    {name: 'Debugging',         a: 11,  q: 12},
-    {name: 'Data Struc',        a: 11,  q: 20},
+    {name: 'Basic JS',          a: 110, q: 110},
+    {name: 'ES6',               a: 31,  q: 31},
+    {name: 'REGEX',             a: 22,  q: 33},
+    {name: 'Debugging',         a: 12,  q: 12},
+    {name: 'Data Struc',        a: 20,  q: 20},
     {name: 'Bsc Algo Scripts',  a: 10,  q: 16},
     {name: 'OOP',               a: 18,  q: 26},
     {name: 'FP',                a: 8,   q: 24},
